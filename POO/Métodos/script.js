1ea57a5f0655004d2dbda161d7aa138e7e62c1b2
lhucaseduardo <lhucasedu@gmail.com>
2022-06-20 20:53:49 -0300

    Adicionado alguns arquivos

---
+++ POO/Métodos/script.js
@@ -0,0 +1,9 @@
+class spaceships {
+    constructor (nome, categoria){
+        this.nome = nome
+        this.categoria = categoria
+    }
+}
+
+let golias = new spaceships("Golias", "Batalha")
+console.log(golias)
